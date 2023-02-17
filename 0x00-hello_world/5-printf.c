9319877765c37aa265e3fbe0743a49b03c9ee969
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-16 18:47:55 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*Just try'na print some stuff*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
