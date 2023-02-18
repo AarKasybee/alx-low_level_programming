8359bdba3104edbeda87e786517c06b574f39fca
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-17 18:56:22 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: reuturns 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i++);
+	}
+	printf("\n");
+	return (0);
+}
