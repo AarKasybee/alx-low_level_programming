c34310d8e7a4fe9ec367a804e27a466a67f153e0
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-20 13:14:30 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 /**
- * main starts first
+ *main - starts first
  *
- * next is return
+ *Return: next is return
  */
 int main(void)
 {
 	char c = 'B';
 	int i = 10;
-	long int li= 12;
-	long long int lli= 3134;
+	long int li = 12;
+	long long int lli = 3134;
 	float f = 4.2;
-	
+
 	printf("Size of a char: %d", sizeof(c));
-	printf(" bytes\n");
+	printf(" byte(s)\n");
 	printf("Size of an int: %d", sizeof(i));
-	printf(" bytes\n");
+	printf(" byte(s)\n");
 	printf("Size of a long int: %d", sizeof(li));
-	printf(" bytes\n");
+	printf(" byte(s)\n");
 	printf("Size of a long long int: %d", sizeof(lli));
-	printf(" bytes\n");
+	printf(" byte(s)\n");
 	printf("Size of a float: %d", sizeof(f));
-	printf(" bytes\n");
+	printf(" byte(s)\n");
 	return (0);
 }
