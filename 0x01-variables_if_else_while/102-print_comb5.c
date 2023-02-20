30102d28ca5200673b27b1e21061b0f41d6cfecb
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-19 18:28:41 -0800

    This will not work. I am just using it as a place holder for me to come and continue

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ *
+ * Return: end
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int m = 0;
+
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+		}
+		while(k < 10)
+		{
+			putchar(k + '0');
+			putchar(m + '0');
+		}
+	}
+	return (0);
+	/* This is not done and is not correct yet*/
+}
+
