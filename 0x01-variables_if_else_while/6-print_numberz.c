1d2c6ea0198723dda0a05ede6eab38517da89c7b
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-19 17:00:47 -0800

    6,9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-        int i = 0;
+	int i = 0;
 	int numArr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        while (i < 10)
-        {
-                putchar((char)numArr[i] + '0');
-                i++;
-        }
-        putchar('\n');
-        return (0);
+	while (i < 10)
+	{
+		putchar((char)numArr[i] + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
