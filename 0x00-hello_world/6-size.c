854c11a8a98a62a405da6ae74cb95fa0357a3dea
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-20 13:06:56 -0800

    using sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,20 +7,21 @@
  */
 int main(void)
 {
-	char c = "Beet";
+	char c = 'B';
 	int i = 10;
 	long int li= 12;
 	long long int lli= 3134;
 	float f = 4.2;
-	printf("Size of a char: ", sizeof(c));
+	
+	printf("Size of a char: %d", sizeof(c));
 	printf(" bytes\n");
-	printf("Size of an int: ", sizeof(i));
+	printf("Size of an int: %d", sizeof(i));
 	printf(" bytes\n");
-	printf("Size of a long int: ", sizeof(li));
+	printf("Size of a long int: %d", sizeof(li));
 	printf(" bytes\n");
-	printf("Size of a long long int: ", sizeof(lli));
+	printf("Size of a long long int: %d", sizeof(lli));
 	printf(" bytes\n");
-	printf("Size of a float: ", sizeof(f));
-	printf(" bytes"\n");
+	printf("Size of a float: %d", sizeof(f));
+	printf(" bytes\n");
 	return (0);
 }
