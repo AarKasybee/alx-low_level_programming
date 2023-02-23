5bbe46baba56233c31116a7361ac114541e48fc6
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-22 16:08:58 -0800

    gotcha :)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,23 +15,57 @@ void times_table(void)
 		while (j < 10)
 		{
 			prod = i * j;
-			if (prod < 10)
+			if (j < 10)
 			{
-				putchar(prod + '0');
-				putchar(',');
-				putchar(' ');
-			}
-			else
-			{
-				putchar(prod / 10 + '0');
-				putchar(prod % 10 + '0');
-				putchar(',');
-				putchar(' ');
+				if (prod < 10)
+				{
+					int next_prod = (i * (j+1));
+
+					if ((next_prod) < 10)
+					{
+						if (j == 9)
+						{
+							putchar(prod + '0');
+						}
+						else
+						{
+							putchar(prod + '0');
+							putchar(',');
+							putchar(' ');
+							putchar(' ');
+						}
+					}
+					else
+					{
+						if (j == 9)
+						{
+							putchar(prod + '0');
+						}
+						else
+						{
+							putchar(prod + '0');
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+				else if (j == 9)
+				{
+					putchar(prod / 10 + '0');
+					putchar(prod % 10 + '0');
+				}
+				else
+				{
+					putchar(prod / 10 + '0');
+					putchar(prod % 10 + '0');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			j++;
 		}
 		putchar('\n');
-		j = 0;
 		i++;
+		j = 0;
 	}
 }
