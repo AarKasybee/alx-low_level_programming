41a6616871dee7b1972bcd79dacdf88069b0b6c7
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-27 17:52:10 -0800

    last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print half the string
+ * @str: input
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int length_of_the_string = _strlen(str);
+	int n = (length_of_the_string - 1) / 2;
+	
+	while (i < _strlen(str) + 1)
+	{
+		if (_strlen(str) % 2 != 0)
+		{
+			putchar(str[n]);
+		}
+		else if (i >= _strlen(str) / 2)
+		{
+			putchar(str[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input char
+ *
+ * Return: string value
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
