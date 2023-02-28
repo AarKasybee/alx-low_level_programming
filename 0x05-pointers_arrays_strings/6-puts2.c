f55cef5b89bbee0fa1430cb49d5c7040cef3ac83
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-27 17:29:04 -0800

    02468

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - function
+ * @str: input
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i < _strlen(str))
+	{
+			if (i % 2 == 0)
+			{
+				putchar(str[i]);
+			}
+			i++;
+	}
+	putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input char
+ *
+ * Return: string value
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
