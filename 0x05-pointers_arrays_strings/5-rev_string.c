20594d6c05b2135febf00be43ca61e9ac35099eb
Beethoven Kasompe <kasbeet@gmail.com>
2023-02-27 17:04:50 -0800

    store string in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,19 +5,18 @@
  * rev_string - reverses a string and stores it back
  * @s: input
  */
-
 void rev_string(char *s)
 {
 	int i, j;
-    	char tempchar;
+	char tempchar;
 	int len = _strlen(s);
 
 	for (i = len - 1, j = 0; i > j; i--, j++)
-    	{
+	{
 		tempchar = s[i];
 		s[i] = s[j];
 		s[j] = tempchar;
-    	}
+	}
 }
 
 /**
