07f760ecb09d7aa559c40cb2a24a7eb151314d08
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-01 04:56:45 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * leet - encodes a string into leet code
+ *
+ * @p: pointer to a character
+ *
+ * Return: returns a char val
+ */
+
+char *leet(char *p)
+{
+	int i = 0;
+	int length = strlen(p);
+	char *final;
+
+	final = malloc(length + 1);
+	memset(final, 0, length + 1);
+	strcpy(final, p);
+	while (p[i] != '\0')
+	{
+		if (p[i] == 'a' || p[i] == 'A')
+			final[i] = ('0' + 4);
+		else if (p[i] == 'e' || p[i] == 'E')
+			final[i] =  ('0' + 3);
+		else if (p[i] == 'o' || p[i] == 'O')
+			final[i] = ('0' + 0);
+		else if (p[i] == 't' || p[i] == 'T')
+			final[i] = ('0' + 7);
+		else if (p[i] == 'l' || p[i] == 'L')
+			final[i] = ('0' + 1);
+		else final[i] = p[i];
+		i++;
+	}
+	return (final);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+This a readme file for 0x06. C - More pointers, arrays and strings
+I wanna do this first. 
+7. Mozart composed his music not for the elite, but for everybody
