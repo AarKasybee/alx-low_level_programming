ae3bdf1971c1511d690cf164ab784fbe9800a127
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-01 18:56:10 -0800

    convert to upper.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+int convert(int c);
 
 /**
  * string_toupper - func
@@ -9,4 +10,28 @@
  */
 char *string_toupper(char *a)
 {
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		a[i] = convert(a[i]);
+		i++;
+	}
+		return a;
+}
+/**
+ * convert - converts from lower to upper
+ * @c: input val
+ * Return: int val
+ */
+int convert(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return c - 'a' + 'A';
+	}
+	else
+	{
+		return c;
+	}
 }
