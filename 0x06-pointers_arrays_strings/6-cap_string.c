1ea282595e6294aed98c521bf2b8d363038ef99f
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-02 14:03:26 -0800

    CAPS

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,11 +2,34 @@
 #include <stdlib.h>
 #include "main.h"
 
+int convert(int c);
 /**
  * cap_string - caps
  * @a: input char
  * Return: char output
  */
-char *cap_string(char *)
+char *cap_string(char *a)
 {
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		a[i] = convert(a[i]);
+		i++;
+	}
+	return (a);
+}
+/**
+ * convert - converts from lower to upper
+ * @c: input val
+ * Return: int val
+ */
+int convert(int c)
+{
+	if ((c >= 'a' && c <= 'z'))
+	{
+		return (c - 'a' + 'A');
+	}
+	else
+		return (c);
 }
