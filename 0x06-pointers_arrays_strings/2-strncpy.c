6c88f317f95f65e2d995cc814b67c28745bc2bd0
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-02 12:44:10 -0800

    strncopy version

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,14 +3,16 @@
 #include "main.h"
 
 /**
- * _strncpy - Function
+ * _strncpy - coppies a specified number of characters ina string
  * @dest: str1
  * @src: ste2
  * @n: intger input
+ * Return: returns the coppied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
