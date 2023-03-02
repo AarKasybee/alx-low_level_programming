b4f2ab36e19db148db14984e35da127b2c328a19
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-02 08:17:11 -0800

    strcat version

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -9,7 +9,7 @@
 int main(void)
 {
     char s1[98] = "Hello ";
-    char s2[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n";
+    char s2[] = "World!\n";
     char *ptr;
 
     printf("%s\n", s1);
