b9dc59ce704a7787cde1edff34f04f622ae5c4ba
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-01 18:23:30 -0800

    Hello world concatenated with strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,4 +11,29 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int dest_len = _strlen(dest);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+	return dest;
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: input char
+ *
+ * Return: string value
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
