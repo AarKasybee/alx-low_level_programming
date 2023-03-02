99e30f1c930e98d6643a643c029896ef9d394aef
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-01 18:10:27 -0800

    Hello world concatenated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * _strcat - concatenates two strings.
@@ -11,4 +13,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char* dest_end = dest + strlen(dest);
+
+	while (*src != '\0')
+	{
+		*dest_end++ = *src++;
+	}
+	*dest_end = '\0';
+	return dest;
 }
