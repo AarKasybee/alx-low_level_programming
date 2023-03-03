25cb0b2f285e4dee6eccd5ad86a03537cc92c06e
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-02 17:48:13 -0800

    numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,31 +15,27 @@ void print_number(int n)
 		putchar('-');
 		n = -n;
 	}
-	if (n >= 10000)
+	if (n >= 1000)
 	{
-		;
+		print_digits(n / 1000);
+		n %= 1000;
+		print_digits(n);
 	}
-	else
+	if (n >= 100)
 	{
-		if (n == 0)
-		{
-			putchar('0' + 0);
-		}
-		if (n >= 1000)
-		{
-			print_digits(n / 1000);
-			n %= 1000;
-		}
-		if (n >= 100)
-		{
-			print_digits(n / 100);
-			n %= 100;
-		}
-		if (n >= 10)
-		{
-			print_digits(n / 10);
-			n %= 10;
-		}
+		print_digits(n / 100);
+		n %= 100;
+		print_digits(n);
+	}
+	if (n >= 10)
+	{
+		print_digits(n / 10);
+		n %= 10;
+		print_digits(n);
+	}
+	if (n == 0)
+	{
+		putchar('0' + 0);
 		print_digits(n);
 	}
 	putchar('\n');
