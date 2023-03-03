7775250c5dfc0c6baf5ddb2928a4b36456d3bc76
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-02 17:32:38 -0800

    numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,91 +3,52 @@
 #include <stdio.h>
 #include <string.h>
 
-void print1s(int n);
-void print10s(int n);
-void print100s(int n);
-void print1000s(int n);
-void printnegs(int n);
+void print_digits(int n);
 /**
  * print_number - prints a num
  * @n: input
  */
 void print_number(int n)
 {
-	if (n == 0)
-	{
-		putchar('0' + 0);
-	}
-	printnegs(n);
-	print1s(n);
-	print10s(n);
-	print100s(n);
-	print1000s(n);
-}
-
-void printnegs(int n)
-{
 	if (n < 0)
 	{
 		putchar('-');
-		if (n < 0 && n >= 9)
+		n = -n;
+	}
+	if (n >= 10000)
+	{
+		;
+	}
+	else
+	{
+		if (n == 0)
 		{
-			putchar('0' + n);
+			putchar('0' + 0);
 		}
-		else if (n >= 10 && n < 100)
+		if (n >= 1000)
 		{
-			int d = n / 10;
-			int r = n % 10;
-
-			putchar('0' + d);
-			putchar('0' + r);
+			print_digits(n / 1000);
+			n %= 1000;
 		}
+		if (n >= 100)
+		{
+			print_digits(n / 100);
+			n %= 100;
+		}
+		if (n >= 10)
+		{
+			print_digits(n / 10);
+			n %= 10;
+		}
+		print_digits(n);
 	}
+	putchar('\n');
 }
-
-void print1s(int n)
+/**
+ * print_digits - prints a num
+ * @n: input
+ */
+void print_digits(int n)
 {
-	if (n >= 0 && n <= 9)
-	{
-		 putchar('0' + n);
-	}
-}
-
-void print10s(int n)
-{
-	if (n >= 10 && n < 100)
-	{
-		int d = n / 10;
-		int r = n % 10;
-		
-		putchar('0' + d);
-		putchar('0' + r);
-	}
-}
-
-void print100s(int n)
-{
-	if (n >= 100 && n < 1000)
-	{
-		int d = n / 100;
-		int r1 = n % 100;
-		int d2 = r1 / 10;
-		int r2 = r1 % 10;
-
-		putchar('0' + d);
-		putchar('0' + d2);
-		putchar('0' + r2);
-	}
-}
-
-void print1000s(int n)
-{
-	 if (n >= 1000 && n < 10000)
-	 {
-		int d = n / 1000;
-		int r1 = n % 1000;	
-		putchar('0' + d);
-		print100s(r1);
-	 }
-	
+	putchar('0' + (n % 10));
 }
