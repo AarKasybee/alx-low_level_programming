73936ba6503ff7a38d6e5ce87f70b9bc55576e0c
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 15:28:07 -0800

    memscpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,8 +4,23 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a
+ * _memcpy - copies memory
+ * @dest: dstination
+ * @src: source
+ * @n: number of bytes
+ * Return: returns a pointer to a char string
  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	char *destination = dest;
+	char *source = src;
 
+	while (i < n)
+	{
+		destination [i] = source [i];
+		i++;
+	}
+	dest = destination;
+	return (dest);
+}
