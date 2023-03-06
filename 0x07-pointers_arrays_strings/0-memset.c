2f575c143b5a2bdbb41913cac1b0944231733b42
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 15:11:03 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,21 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s:pointer to a char
+ * @b: constant byte
+ * @n: first number of bytes
+ * Return: returns a char of the memory area
  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *memblock = s;
+	unsigned int i = 0;
 
+	while (i < n)
+	{
+		memblock[i] = b;
+		i++;
+	}
+	return (s);
+}
