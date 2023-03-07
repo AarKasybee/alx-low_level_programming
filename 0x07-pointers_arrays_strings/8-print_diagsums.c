2a6d9ed38846e0d62271238c05de6e54ad3df760
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 17:39:12 -0800

    ragged diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,8 +4,24 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a 
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers
+ * @a: square matrix of which we print the sum of diagonals
+ * @size: size of the matrix
  */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
 
+	unsigned int sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	while (i < size)
+	{
+		sum1 += a[(size * i) + i];
+		sum2 += a[(size * (i + 1)) - (i + 1)];
+		i++;
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
