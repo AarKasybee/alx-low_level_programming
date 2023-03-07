d9e01eb44481fcf230063519996b0f13c1984d02
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 16:38:31 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,8 +4,29 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a
+ * _strspn - gets the length of a prefix substring.
+ * @s: intial string segment
+ * @accept: bytes to be used
+ * Return: returns an unsigned int
  */
+unsigned int _strspn(char *s, char *accept)
+{
+	size_t i, j;
 
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (!accept[j])
+		{
+			break;
+		}
+	}
+	i = (unsigned int) i;
+	return (i);
+}
