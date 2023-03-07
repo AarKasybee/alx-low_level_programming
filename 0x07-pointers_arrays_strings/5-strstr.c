b84311693869a9f3ed46eccd94a7c4b12aba14d4
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 17:10:32 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,8 +4,28 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a
+ * _strstr - finds the first occurrence of the substring needle in the string haystack
+ * @haystack: string
+ * @needle: string
+ * Return: returns a string
  */
+char *_strstr(char *haystack, char *needle)
+{
+	char *p;
+	char *q;
+	char *r;
+
+	for (p = haystack; *p != '\0'; p++)
+	{
+		for (q = p, r = needle; *r != '\0' && *q == *r; q++, r++)
+		{
+			;
+		}
+		if (*r == '\0')
+		{
+			return p;
+		}
+	}
+	return NULL;
+}
 
