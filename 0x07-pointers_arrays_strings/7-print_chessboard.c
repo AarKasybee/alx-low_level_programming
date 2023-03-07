9a2e5158c3660628fc0a6dedab35a76684aeadd4
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 17:27:27 -0800

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,8 +4,20 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a
+ * print_chessboard - prints chessboard
+ * @a: arr
+ * Return: void
  */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
 
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			putchar(a[i][j]);
+		}
+		putchar('\n');
+	}
+}
