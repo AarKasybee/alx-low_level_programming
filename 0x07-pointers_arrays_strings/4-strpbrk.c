06096cdb99f2d220a81090ee0d4ab35f98c0cfee
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 17:00:27 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,8 +4,25 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
- * Return: returns a
+ * _strpbrk - compares two strings
+ * @s: string 1
+ * @accept: string 2
+ * Return: returns a string
  */
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+	char *q;
 
+	for (p = s; *p != '\0'; p++)
+	{
+		for (q = accept; *q != '\0'; q++)
+		{
+			if (*p == *q)
+			{
+				return (char *)p;
+			}
+		}
+	}
+	return NULL;
+}
