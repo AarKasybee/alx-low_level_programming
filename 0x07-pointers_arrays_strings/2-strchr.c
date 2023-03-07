92c3e680d51631a5eca08562fd4df6e141ff306a
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-06 16:03:50 -0800

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,8 +4,23 @@
 #include "main.h"
 
 /**
- * Func
- * @c:
+ * _strchr - locates a character in a string.
+ * @c: firs character
+ * @s: point to a char string
  * Return: returns a
  */
+char *_strchr(char *s, char c)
+{
+	size_t i = 0;
+	size_t len = strlen(s);
 
+	while (i < len)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
