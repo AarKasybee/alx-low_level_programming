cf69cba58881a7ff512f668f5f99b0f4008f317c
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-10 14:35:47 -0800

    print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - sartingpoint
+ * @argc: arguiment counter
+ * @argv: string vector
+ *
+ * Return: 0 integer
+ */
+int main (int argc, char *argv[])
+{
+	if (argc < 1)
+	{
+		exit(1);
+	}
+	printf ("\"%s\".\n",argv[0]);
+	return (0);
+}
