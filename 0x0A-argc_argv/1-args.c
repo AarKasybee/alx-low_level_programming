e242527a7975190fd7a74d2f8bdb9529fa6d5beb
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-10 15:35:24 -0800

    num of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: indicates the number of arguments
+ * @argv: points to the nth arguments
+ *
+ * Return: success is 0
+ */
+int main(int argc, char *argv[])
+{	
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
