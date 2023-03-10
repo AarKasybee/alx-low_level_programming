f7c7464ccf2674dd5afba74331f1a89db5cda3fa
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-10 14:41:27 -0800

    arguments passed to main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,12 +8,12 @@
  *
  * Return: 0 integer
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc < 1)
 	{
 		exit(1);
 	}
-	printf ("\"%s\".\n",argv[0]);
+	printf("\"%s\".\n", argv[0]);
 	return (0);
 }
