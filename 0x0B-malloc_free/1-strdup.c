40e300b2d8b116b7646b335bac2d24298c964c45
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-14 18:02:45 -0700

    string duplication with dynamic mem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string input
+ * Return: pointer to a newly allocated space in memory,
+ */
+char *_strdup(char *str)
+{
+	char *newStr;
+	int size = 0;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	newStr = (char*) malloc((size + 1) * sizeof(char));
+	if (newStr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		newStr[i] = str[i];
+	}
+	return (newStr);
+
+}
