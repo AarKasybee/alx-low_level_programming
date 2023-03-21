92644ecf43a9dd9de208b0a21eb437307ca261e1
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-20 19:44:20 -0700

    My first c struct - dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+/**
+ * dog - dog info
+ * @name: dog's name
+ * @age: dog's age in dog years :)
+ * #owner: dog's owner
+ *
+ * Description: This gives the details of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
