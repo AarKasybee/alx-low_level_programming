e08470d80e668f6ff6e08c74d94694e4ecdee799
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-20 20:40:28 -0700

    define a struct tp be used in a function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * init_dog - god info
  * @dog: struct name
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
 #ifndef MY_HEADER_FILE_H
 #define MY_HEADER_FILE_H
+
 /**
  * dog - dog info
  * @name: dog's name
  * @age: dog's age in dog years :)
  * #owner: dog's owner
  *
- * Description: This gives the details of the dog
+ * Description: This gives the details of the dog, name, age and owner
  */
 struct dog
 {
@@ -14,4 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
