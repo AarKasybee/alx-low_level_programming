3712ff65f09dccd920c18cb426efef43771679ca
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-23 19:32:34 -0700

    printing numbers using a variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,43 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers
+ * print_numbers - prints numbers given as parameters
  * @separator: string to be printed between numbers
- * @n: input int arguments
+ * @n: number of integers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
 	unsigned int i;
-	unsigned int j;
+	va_list list;
 
-	va_start(args, n);
-	if (separator != NULL)
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < (n); i++)
-		{
-			j = va_arg(args, int);
-			printf("%u ", j);
-			if (i < n - 1)
-			{
-				printf("%s", separator);
-			}
-		}
-		printf("\n");
-		va_end(args);
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
-	else
-	{
-		for (i = 0; i < (n); i++)
-		{
-			j = va_arg(args, int);
-			 printf("%u ", j);
-		}
-		printf("\n");
-		va_end(args);
-	}
+
+	va_end(list);
+
+	printf("\n");
 }
