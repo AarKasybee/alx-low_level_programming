c13ab861b8566a9e15fdcb3ee0f09f3976ab2d12
Beethoven Kasompe <kasbeet@gmail.com>
2023-03-31 19:12:17 -0700

    ASM code "Hello, Holberton"

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
+#include <stdio.h>
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
@@ -15,12 +15,14 @@ list_t *add_node(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	while (str[len])
+	{
 		len++;
-
+	}
 	new = malloc(sizeof(list_t));
 	if (!new)
+	{	
 		return (NULL);
-
+	}
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
