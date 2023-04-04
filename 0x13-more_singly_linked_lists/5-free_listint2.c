2ff7fe892596886fb139e8471a8c11b42cdc8db2
Beethoven Kasompe <kasbeet@gmail.com>
2023-04-03 18:49:52 -0700

    free linked list - approach 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: pointer to a struct
+ *
+ * Returns: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node = *head;
+
+	while (current_node != NULL)
+	{
+		listint_t *next_node = current_node->next;
+
+		free(current_node);
+		current_node = next_node;
+	}
+	*head = NULL;
+}
