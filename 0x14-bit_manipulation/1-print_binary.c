d1c37bfb59c96eeabef9003cfa38fd802b126e2f
Beethoven Kasompe <kasbeet@gmail.com>
2023-04-08 19:15:46 -0700

    dec-bin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,7 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return;
 	}
 	while (temp > 0)
@@ -30,7 +30,7 @@ void print_binary(unsigned long int n)
 		}
 		else
 		{
-			_putchar('0')
+			_putchar('0');
 		}
 		shifts--;
 	}
