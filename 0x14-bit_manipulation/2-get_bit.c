ae0bf929857f46afb2d096c70f86e8428edf1b6b
root <Beethoven Kasompe>
2023-05-29 18:56:04 -0700

    mods

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,14 +13,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	 unsigned long int mask;
-	  unsigned long int bit;
+	unsigned long int mask;
+	unsigned long int bit;
 
-	  if (index >= sizeof(unsigned long int) * 8)
-	  {
-		  return (-1);
-	  }
-	  mask = 1UL << index;
-	  bit = (n & mask) >> index;
-	  return ((int)bit);
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	mask = 1UL << index;
+	bit = (n & mask) >> index;
+	return ((int)bit);
 }
