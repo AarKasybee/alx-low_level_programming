9f17a089462f265498c6e47997507b41e54bfb16
root <Beethoven Kasompe>
2023-05-29 18:36:43 -0700

    handle ULONG_MAX

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,31 +9,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp = n;
-	int shifts = 0;
-	int started = 0;
+	unsigned long int temp;
+	int shifts;
 
 	if (n == 0)
 	{
 		printf("0");
 		return;
 	}
-	while (temp > 0)
-	{
-		temp >>= 1;
-		shifts++;
-	}
-	while (shifts >= 0)
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
 	{
 		if ((n >> shifts) & 1)
-		{
-			started = 1;
 			printf("1");
-		}
-		else if (started)
-		{
+		else
 			printf("0");
-		}
-		shifts--;
 	}
 }
